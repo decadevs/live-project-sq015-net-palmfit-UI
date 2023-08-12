6046c24eb12efe4738371aa2d93a9032f7155cb3
James Adah <adahjames11@gmail.com>
2023-08-12 22:12:27 +0100

    added styling to the subcription page

--- src/components/subcription/Subcription.jsx
+++ src/components/subcription/Subcription.jsx
@@ -1,29 +1,32 @@
-import styles from "./Subcription.module.css"
+import styles from "./Subcription.module.css";
 
 function Subcription() {
   return (
-    <div className={styles.container}>
-      <h3>Active plan</h3>
-      <p>
-        You are currently not subscribed to any plan. Check below for our
-        pricing to get started.
-      </p>
-      <div>
-        <button className={styles.btn} type="button">
-          Subscribe
-        </button>
-      </div>
+      <div className={styles.container}>
+          <div className={styles.content_container}>
+              
+                <h3>Active plan</h3>
+                <p>
+                    You are currently not subscribed to any plan. Check below for our
+                    pricing to get started.
+                </p>
+                <div className={styles.btn_con}>
+                    <button className={styles.btn} type="button">
+                    Subscribe
+                    </button>
+                </div>
 
-      <div className={styles.wallet_container}>
-        <div className={styles.top_half}>
-          <span>Palmfit Wallet</span>
-          <span>#29,500</span>
-              </div>
-              <span>paid out</span>
+                <div className={styles.wallet_container}>
+                    <div className={styles.top_half}>
+                    <span>Palmfit Wallet</span>
+                    <span>#29,500</span>
+                    </div>
+                    <span className={styles.paid}>paid out</span>
+                </div>
+                <button type="button" className={styles.btn_secondary}>Fund Wallet</button>
           </div>
-          <button type="button" >Fund Wallet</button>
     </div>
   );
 }
 
-export default Subcription
+export default Subcription;
