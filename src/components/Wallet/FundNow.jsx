80c5e82033f774b16d9c09ed9b2810dac9c155d4
CalebTek <olawale.odeyemi@decagon.dev>
2023-08-13 14:28:00 +0100

    Create Wallet Component folder
    1. Added FundNow.jsx
    2. Added FundSuccess.jsx
    3. Added FundWallet.jsx

---
+++ src/components/Wallet/FundNow.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import FundSuccess from './FundSuccess';
+
+const FundNow = () => {
+  const [paymentSuccessful, setPaymentSuccessful] = useState(false);
+
+  const handlePayment = () => {
+    setPaymentSuccessful(true);
+  };
+
+  if (paymentSuccessful) {
+    return <FundSuccess />;
+  }
+
+  return (
+    <div className="pay-now-container">
+            <h1>Get unlimited access to everything</h1>
+      <p>Enter your credit card details to complete wallet funding</p>
+      <label>Amount</label>
+      <input type="text" placeholder="Enter amount in number" />
+      <label>Card Number</label>
+      <input type="text" placeholder="1234 1234 1234 1234" />
+      <div className="input-group">
+        <div>
+        <label>Expiration date</label>
+        <input type="text" placeholder="MM/YY" />
+        </div>
+        <div>
+        <label>Security code</label>
+        <input type="text" placeholder="123" />
+        </div>
+      </div>
+      <p className="terms">
+        By subscribing you agree to our Use of Service and you'll have access to all locked features until 20 Oct 2021.
+      </p>
+      <button onClick={handlePayment} className="pay-now-button">Pay Now</button>
+    </div>
+  );
+};
+
+export default FundNow;
