112cf6cf739d0c106516803d2813a863337904c2
David Ogwuche <133539148+codedvd@users.noreply.github.com>
2023-08-15 13:07:47 +0100

    Added the Component and style for Calculating Calories

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
+import { CalculateCalories } from './components/CalculateCalories';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+
+    <CalculateCalories />
+    {/* <App /> */}
   </React.StrictMode>
 );
 
