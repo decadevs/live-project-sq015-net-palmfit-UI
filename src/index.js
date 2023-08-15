4ae0a483299cfd91bad0150e0639118f7b3b7b4e
Unekwu001 <unekwutheophilus@gmail.com>
2023-08-15 10:55:29 +0100

    commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
+import SignUpPage1 from './components/SignUpPage1';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+         <App />
+        {/*<SignUpPage1/>*/}
   </React.StrictMode>
 );
 
