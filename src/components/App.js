29e94612cc076c68d207947337e132345d3390ac
Chiheart94 <martasims94@gmail.com>
2023-08-16 16:41:12 +0100

    commit meal-list page

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,30 @@
+import React from 'react';
 import '../styles/App.css';
 
+
+import MealInfo from './Meal-List-Plan/MealInfo';
+import FoodTable from './Meal-List-Plan/FoodTable';
+import FoodSearch from './Meal-List-Plan/FoodSearch';
+import '../styles/Style.css';
+
+
 function App() {
+  const foods = [
+    { name: 'Plain white rice', amount: 2.5, measurement: 'cup', calories: 600, caloriesPerMeasurement: { cup: 100, tablespoon: 50, ounce: 75 } },
+    //{ name: 'Food 2', amount: 2, measurement: 'tablespoon', calories: 50, caloriesPerMeasurement: { cup: 100, tablespoon: 50, ounce: 75 } },
+    //{ name: 'Food 3', amount: 2, measurement: 'tablespoon', calories: 50, caloriesPerMeasurement: { cup: 100, tablespoon: 50, ounce: 75 } },
+    //{ name: 'Food 3', amount: 2, measurement: 'tablespoon', calories: 50, caloriesPerMeasurement: { cup: 100, tablespoon: 50, ounce: 75 } },
+    // Add more food items
+  ];
+
   return (
-    <div> Hello World</div>
+    <div className="App">
+      <MealInfo mealName="Meal 1" totalCalories={foods.reduce((total, food) => total + parseFloat(food.calories), 0)} />
+      <FoodTable foods={foods} />
+      { <FoodSearch/> }
+    </div>
   );
 }
 
 export default App;
+
---
+++ src/components/Meal-List-Plan/FoodTable.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+//import './FoodTable.css';
+
+const FoodTable = ({ foods }) => {
+  const [foodData, setFoodData] = useState(foods);
+
+  return (
+    <>
+    <table className="food-table">
+      <thead>
+        <tr>
+          <th>FOOD</th>
+          <th>AMOUNT</th>
+          <th>CALORIES</th>
+        </tr>
+      </thead>
+      <tbody>
+        {foodData.map((food, index) => (
+          <tr key={index}>
+            <td>{food.name}</td>
+            <td>
+              <input readOnly  type="number" value={food.amount} />
+               <select  value={food.measurement}>
+                <option value="cup">cup</option>   
+              </select>
+            </td>
+            <td>{parseFloat(food.calories, 0)}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+        <div className="total-row">
+          <p>Total</p>
+          <p>{foodData.reduce((total, food) => total + parseFloat(food.calories), 0)}</p>
+        </div>
+    </>
+  );
+};
+
+export default FoodTable;
