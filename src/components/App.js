f4430daef151d06aa2ea75108e3ff408255bddd1
Asimole Martha <martasims94@gmail.com>
2023-08-21 00:24:54 +0100

    getting update from develop

--- src/components/App.js
+++ src/components/App.js
@@ -10,8 +10,8 @@ import '../styles/Style.css';
 
 function App() {
   const foods = [
-    { name: 'Plain white rice', amount: 2.5, measurement: 'cup', calories: 600, caloriesPerMeasurement: { cup: 100, tablespoon: 50, ounce: 75 } },
-    //{ name: 'Food 2', amount: 2, measurement: 'tablespoon', calories: 50, caloriesPerMeasurement: { cup: 100, tablespoon: 50, ounce: 75 } },
+    { name: 'Plain white rice', amount: 2.5, measurement: 'cup', calories: 605, caloriesPerMeasurement: { cup: 100, tablespoon: 50, ounce: 75 } },
+    // { name: 'Food 2', amount: 2, measurement: 'tablespoon', calories: 50, caloriesPerMeasurement: { cup: 100, tablespoon: 50, ounce: 75 } },
     //{ name: 'Food 3', amount: 2, measurement: 'tablespoon', calories: 50, caloriesPerMeasurement: { cup: 100, tablespoon: 50, ounce: 75 } },
     //{ name: 'Food 3', amount: 2, measurement: 'tablespoon', calories: 50, caloriesPerMeasurement: { cup: 100, tablespoon: 50, ounce: 75 } },
     // Add more food items
@@ -19,7 +19,7 @@ function App() {
 
   return (
     <div className="App">
-      <MealInfo mealName="Meal 1" totalCalories={foods.reduce((total, food) => total + parseFloat(food.calories), 0)} />
+      <MealInfo mealName="Meal 1 name" totalCalories={foods.reduce((total, food) => total + parseFloat(food.calories), 0)} />
       <FoodTable foods={foods} />
       { <FoodSearch/> }
     </div>
