43166a8daed3590a58114b885e40e4811302ce9a
tob-t <nworgu.tobechukwu@gmail.com>
2023-08-20 20:30:26 +0100

    Added the food single page

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import '../styles/App.css';
+import FoodSingleOne from '../components/components/meal/FoodSingleOne';
 
 function App() {
   return (
-    <div> Hello World</div>
+    // <div> Hello World</div>
+    <FoodSingleOne />
   );
+  
 }
 
 export default App;
