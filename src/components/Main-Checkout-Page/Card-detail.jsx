6dd9d2eb17dad5f191c9fdccc648fdcb6a08ea48
CalebTek <olawale.odeyemi@decagon.dev>
2023-08-21 09:41:20 +0100

    Add Main-Checkout-Component

---
+++ src/components/Main-Checkout-Page/Card-detail.jsx
@@ -0,0 +1,185 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCcMastercard,
+  faCcVisa,
+  faCcPaypal
+} from "@fortawesome/free-brands-svg-icons";
+
+const CardDetail = ({
+  subtotal,
+  couponDiscount,
+  total,
+  onCouponApply,
+  couponCode,
+  onCouponCodeChange
+}) => {
+  const cardTypes = ["Mastercard", "Visa", "PayPal", "PayStack", "Payoneer"];
+
+  const cardTypeIcons = {
+    Mastercard: faCcMastercard,
+    Visa: faCcVisa,
+    PayPal: faCcPaypal
+    //PayStack: faSomePayStackIcon,
+    //Payoneer: faSomePayoneerIcon
+    // ... Add other icons for each card type
+  };
+
+  const [paymentSuccessful, setPaymentSuccessful] = useState(false);
+  const [cardType, setCardType] = useState("");
+  const [cardholderName, setCardholderName] = useState("");
+  const [cardNumber, setCardNumber] = useState("");
+  const [expiration, setExpiration] = useState("");
+  const [cvv, setCVV] = useState("");
+
+  const handlePayment = () => {
+    // Perform payment processing logic
+    setPaymentSuccessful(true);
+  };
+
+  if (paymentSuccessful) {
+    return <div>Payment Successful! Thank you for your purchase.</div>;
+  }
+
+  return (
+    <>
+      <div
+        className="card-details text-muted"
+        style={{
+          color: "#000000",
+          fontFamily: "Nunito Sans-SemiBold, Helvetica",
+          fontSize: "28px",
+          fontWeight: 600,
+          marginBottom: "20px",
+          textAlign: "center"
+        }}
+      >
+        Card details
+      </div>
+
+      <div
+        className="check-out mb-3"
+        style={{
+          backgroundColor: "#1A8D8D",
+          padding: "20px",
+          borderRadius: "5px"
+        }}
+      >
+        <div className="mb-3">
+          {/* Card Type */}
+          <div className="mb-3">
+            <label>Card type</label>
+            <div className="d-flex">
+              {cardTypes.map((type) => (
+                <a
+                  href="#!"
+                  className="text-white me-2"
+                  key={type}
+                  onClick={() => setCardType(type)}
+                >
+                  <FontAwesomeIcon icon={cardTypeIcons[type]} size="2x" />
+                </a>
+              ))}
+            </div>
+          </div>
+
+          {/* Cardholder's Name */}
+          <input
+            type="text"
+            className="form-control form-control-lg mb-3"
+            placeholder="Cardholder's Name"
+            value={cardholderName}
+            onChange={(e) => setCardholderName(e.target.value)}
+          />
+
+          {/* Card Number */}
+          <input
+            type="text"
+            className="form-control form-control-lg mb-3"
+            placeholder="1234 5678 9012 3457"
+            value={cardNumber}
+            onChange={(e) => setCardNumber(e.target.value)}
+            maxLength="19"
+          />
+
+          {/* Expiration and CVV */}
+          <div className="mb-3">
+            <div className="d-flex justify-content-between mb-md-0">
+              <div className="me-md-3 mb-3 mb-md-0">
+                <label>Expiration</label>
+                <input
+                  type="text"
+                  className="form-control form-control-lg"
+                  placeholder="MM/YY"
+                  value={expiration}
+                  onChange={(e) => setExpiration(e.target.value)}
+                  maxLength="5"
+                />
+              </div>
+              <div>
+                <label>CVV</label>
+                <input
+                  type="text"
+                  className="form-control form-control-lg"
+                  placeholder="123"
+                  value={cvv}
+                  onChange={(e) => setCVV(e.target.value)}
+                  maxLength="3"
+                />
+              </div>
+            </div>
+          </div>
+
+          {/* Line Separator */}
+          <hr className="my-4" style={{ backgroundColor: "white" }} />
+
+          {/* Price and Total */}
+          <div className="mb-3 subtotal">
+            <div className="mb-3 d-flex justify-content-between">
+              <label>Subtotal</label>
+              <div className="subtotal-amount">
+                ${parseFloat(subtotal.toFixed(2))}
+              </div>
+            </div>
+            <div className="mb-3 d-flex justify-content-between">
+              <label>Coupon</label>
+              <div className="d-flex justify-content-between">
+                <input
+                  type="text"
+                  className="form-control coupon-input"
+                  placeholder="Enter coupon code"
+                  value={couponCode}
+                  onChange={(e) => onCouponCodeChange(e.target.value)}
+                />
+                <button
+                  className="btn btn-info btn-sm ms-2 coupon-button"
+                  onClick={onCouponApply}
+                >
+                  Apply
+                </button>
+              </div>
+            </div>
+            <div className="d-flex justify-content-between">
+              <label>Total (Incl. taxes)</label>
+              <div>${parseFloat(total.toFixed(2))}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="d-flex justify-content-center">
+        {/* Checkout Button */}
+        <button
+          type="button"
+          className="btn btn-info btn-block btn-lg checkout-button"
+          onClick={handlePayment}
+        >
+          <div className="d-flex justify-content-between">
+            <span className="text-end">Checkout</span>
+          </div>
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default CardDetail;
---
+++ src/components/Main-Checkout-Page/CheckOutPage.jsx
@@ -0,0 +1,187 @@
+import React from "react";
+import CardDetail from "./Card-detail";
+import SelectedFeatures from "./Selected-Feature";
+import { useState } from "react";
+import personalizedWorkoutPlansImage from "../../../src/images/personalized-workout-plans.jpg";
+import nutritionTrackingImage from "../../../src/images/nutrition-tracking.jpg";
+import progressTrackingImage from "../../../src/images/progress-tracking.jpg";
+
+import "./CheckOut.css";
+
+const Checkout = () => {
+  const initialFeatures = [
+    {
+      name: "Personalized Workout Plans",
+      description:
+        "Customized workout plans tailored to your goals and fitness level.",
+      image: personalizedWorkoutPlansImage,
+      quantity: 1,
+      price: 9.99
+    },
+    {
+      name: "Nutrition Tracking",
+      description:
+        "Keep track of your daily meals and calories to support your fitness journey.",
+      image: nutritionTrackingImage,
+      quantity: 1,
+      price: 7.99
+    },
+    {
+      name: "Progress Tracking",
+      description:
+        "Track your progress with weight, body measurements, and fitness achievements.",
+      image: progressTrackingImage,
+      quantity: 1,
+      price: 5.99
+    }
+  ];
+
+  const [features, setFeatures] = useState(initialFeatures);
+  const [couponCode, setCouponCode] = useState("");
+  const [subtotal, setSubtotal] = useState(
+    features.reduce((total, feature) => total + feature.price, 0)
+  );
+  const [couponDiscount, setCouponDiscount] = useState(0);
+  const [total, setTotal] = useState(subtotal - couponDiscount);
+
+  const handleDeleteFeature = (index) => {
+    const updatedFeatures = features.filter((_, i) => i !== index);
+    setFeatures(updatedFeatures);
+    recalculateTotals(updatedFeatures, couponCode);
+  };
+
+  const handleCouponApply = () => {
+    // Apply coupon logic here, update couponDiscount and recalculate totals
+    setCouponDiscount(5); // Example: Apply a fixed $5 discount
+    recalculateTotals(features, couponCode);
+  };
+
+  const handleCouponCodeChange = (code) => {
+    setCouponCode(code);
+  };
+
+  const recalculateTotals = (updatedFeatures, couponCode) => {
+    const updatedSubtotal = updatedFeatures.reduce(
+      (total, feature) => total + feature.price,
+      0
+    );
+    setSubtotal(updatedSubtotal);
+
+    // Calculate the discount based on the couponCode and update couponDiscount
+    let updatedCouponDiscount = 0;
+    if (couponCode === "DISCOUNT5") {
+      updatedCouponDiscount = 5; // Example: Apply a fixed $5 discount for coupon code DISCOUNT5
+    }
+    setCouponDiscount(updatedCouponDiscount);
+
+    const updatedTotal = updatedSubtotal - updatedCouponDiscount;
+    setTotal(updatedTotal);
+  };
+
+  return (
+    <div className="row">
+      <div className="col-md-6 mb-4">
+        <SelectedFeatures
+          features={features}
+          onDeleteFeature={handleDeleteFeature}
+        />
+      </div>
+      <div className="col-md-6">
+        <CardDetail
+          subtotal={subtotal}
+          couponDiscount={couponDiscount}
+          total={total}
+          onCouponApply={handleCouponApply}
+          couponCode={couponCode}
+          onCouponCodeChange={handleCouponCodeChange}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Checkout;
+
+// import React from "react";
+// import CardDetail from "./Card-detail";
+// import SelectedFeatures from "./Selected-Feature";
+// import { useState } from "react";
+
+// import "./CheckOut.css";
+
+// const Checkout = () => {
+//   const initialFeatures = [
+//     {
+//       name: "Feature 1",
+//       description: "Description for Feature 1",
+//       image:
+//         "https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img1.webp",
+//       quantity: 1,
+//       price: 9.99
+//     },
+//     {
+//       name: "Feature 2",
+//       description: "Description for Feature 2",
+//       image:
+//         "https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img2.webp",
+//       quantity: 1,
+//       price: 19.99
+//     }
+//     // Add more feature objects here
+//   ];
+
+//   const [features, setFeatures] = useState(initialFeatures);
+
+//   const [couponCode, setCouponCode] = useState("");
+
+//   const handleDeleteFeature = (index) => {
+//     const updatedFeatures = features.filter((_, i) => i !== index);
+//     setFeatures(updatedFeatures);
+//   };
+
+//   const calculateSubtotal = () => {
+//     return features.reduce((total, feature) => {
+//       const priceNumeric = parseFloat(feature.price.replace("$", ""));
+//       return total + priceNumeric;
+//     }, 0);
+//   };
+
+//   const subtotal = calculateSubtotal();
+
+//   const [couponDiscount, setCouponDiscount] = useState(0);
+
+//   const total = subtotal - couponDiscount;
+
+//   const handleCouponApply = () => {
+//     // Sample coupon code and its corresponding discount
+//     const coupons = {
+//       SUMMER20: 20, // 20% discount
+//       SALE10: 10 // 10% discount
+//       // ... add more coupons here
+//     };
+
+//     if (coupons.hasOwnProperty(couponCode)) {
+//       const appliedCouponDiscount = (subtotal * coupons[couponCode]) / 100;
+//       setCouponDiscount(appliedCouponDiscount);
+//     } else {
+//       // Invalid coupon code
+//       alert("Invalid coupon code");
+//     }
+//   };
+
+//   return (
+//     <div className="row">
+//       <div className="col-md-6 mb-4">
+//         <SelectedFeatures
+//           features={features}
+//           onDeleteFeature={handleDeleteFeature}
+//         />
+//       </div>
+//       <div className="col-md-6">
+//         <CardDetail subtotal={subtotal} />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Checkout;
