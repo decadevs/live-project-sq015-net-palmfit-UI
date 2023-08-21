260a43dabe91d90c1f84abdc7a6aa03dd945d0f9
zikglobal <125207118+zikglobal@users.noreply.github.com>
2023-08-21 15:16:06 +0100

    Login hot fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
 import App from './components/App';
-import { CalculateCalories } from './components/CalculateCalories';
+//import { CalculateCalories } from './components/CalculateCalories';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -14,8 +14,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 
-    <CalculateCalories />
-    {/* <App /> */}
+    {/* <CalculateCalories /> */}
+    <App />
   </React.StrictMode>
 );
 
