031527908a810c7074702c66b5722a22cd243122
Mistaweng <122454766+Mistaweng@users.noreply.github.com>
2023-08-21 13:48:58 +0100

    Resolved merge conflicts

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
-import { CalculateCalories } from './components/CalculateCalories';
+//import { CalculateCalories } from './components/CalculateCalories';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -12,8 +12,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 
-    <CalculateCalories />
-    {/* <App /> */}
+    {/* <CalculateCalories /> */}
+    <App />
   </React.StrictMode>
 );
 
