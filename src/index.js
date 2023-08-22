669ce974139188d887077c5a9c00f9312ebce62c
Taiwo525 <ayothdav52@gmail.com>
2023-08-22 01:53:57 +0100

    updated Daily-Plan-Breakfast

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import DailyPlanBreakfast from './components/DailyPlanBreakfast';
+import App from './components/App';
+import { CalculateCalories } from './components/CalculateCalories';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 
@@ -8,6 +13,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <DailyPlanBreakfast/>
+
+    <CalculateCalories />
+    {/* <App /> */}
   </React.StrictMode>
 );
 
