b608cc87fa15543630cf24bf9133c4d6d220c04c
James Adah <adahjames11@gmail.com>
2023-08-23 16:52:50 +0100

    landing page view

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,41 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import '../styles/App.css';
+
+//images
+import { logo_dark_svg, menu } from "./navigation/assets";
+
+//components
+import {Header, Footer, Landing} from "./index"
+
+
+
+
+
+
 // import LandingPage from '../components/HomePg/LandingPg';
-// // import '../styles/App.css';
 // import FoodSearch from './FoodSearch';
-// import React, { useState } from 'react';
-// import { NavLink } from 'react-router-dom';
 // import '../styles/App.css';
 // import { ProfilePage } from './ProfilePage';
 
+// import PaymentSubscription from './Payment/Subscription';
+// import FoodSingleOne from '../components/components/meal/FoodSingleOne';
 
-//import '../style/Style.css';
+// import "../styles/App.css";
+// import "../styles/FoodSearch.css";
 
-import MealInfo from "./Meal-List-Plan/MealInfo";
-import FoodTable from "./Meal-List-Plan/FoodTable";
-import SearchFood from "./Meal-List-Plan/SearchFood";
-import Subscription from "./subscription/Subscription";
+// import MealInfo from "./Meal-List-Plan/MealInfo";
+// import FoodTable from "./Meal-List-Plan/FoodTable";
+// import SearchFood from "./Meal-List-Plan/SearchFood";
+// import Subscription from "./subscription/Subscription";
 
-import CalculateCalories from "./CalculateCalories";
+// import CalculateCalories from "./CalculateCalories";
 import Navigation from "./navigation/Navigation";
-import { logo_dark_svg, menu } from "./navigation/assets";
-import MealPlan from "./MealPlans/Mealplan";
-import WeeklyPlanbreakfast from "./weeklyplanbreakfast/WeeklyPlanbreakfast";
-import styles from "../styles/WeeklyPlanbreakfast.module.css";
-import { CalulateCalories } from "../components/CalculateCalories";
-import DailyPlanBreakfast from "./DailyPlan/DailyPlanBreakfast";
+// import MealPlan from "./MealPlans/Mealplan";
+// import WeeklyPlanbreakfast from "./weeklyplanbreakfast/WeeklyPlanbreakfast";
+// import styles from "../styles/WeeklyPlanbreakfast.module.css";
+// import { CalulateCalories } from "../components/CalculateCalories";
+// import DailyPlanBreakfast from "./DailyPlan/DailyPlanBreakfast";
 
 function App() {
   const foods = [
@@ -33,76 +47,44 @@ function App() {
       caloriesPerMeasurement: { cup: 100, tablespoon: 50, ounce: 75 },
     },
 
-    //{ name: 'Food 2', amount: 2, measurement: 'tablespoon', calories: 50, caloriesPerMeasurement: { cup: 100, tablespoon: 50, ounce: 75 } },
-    //{ name: 'Food 3', amount: 2, measurement: 'tablespoon', calories: 50, caloriesPerMeasurement: { cup: 100, tablespoon: 50, ounce: 75 } },
-    //{ name: 'Food 3', amount: 2, measurement: 'tablespoon', calories: 50, caloriesPerMeasurement: { cup: 100, tablespoon: 50, ounce: 75 } },
-    // Add more food items
+ 
   ];
   const [menuIsOpen, setMenuIsOpen] = useState(false);
 
   return (
-    <>
-      {/* <div className="container">
-        <div className="App">
-          <FoodSearch />
+   
 
-          <ProfilePage />
+    <div className="container__app">
+          {/* <div className="nav">
+            <NavLink to={"/"}>
+              <img src={logo_dark_svg} alt="svg logo" className="nav-logo" />
+            </NavLink>
+            <Navigation menuIsOpen={menuIsOpen} setMenuIsOpen={setMenuIsOpen} />
+            <div
+              className="toggle_btn_con"
+              style={{ display: menuIsOpen ? "none" : "block" }}
+              onClick={() => setMenuIsOpen((prev) => !prev)}
+            >
+              {!menuIsOpen && <img src={menu} alt="" className="menu_open" />}
+            </div>
+          </div> */}
 
-          <PaymentSubscription />
-
-          <FoodSingleOne />
-
-          <div className="App">
-            <MealInfo
-              mealName="Meal 1 name"
-              totalCalories={foods.reduce(
-                (total, food) => total + parseFloat(food.calories),
-                0
-              )}
-            />
-            <FoodTable foods={foods} />
+      <div className="pages">
+        <Landing/>
+            {/* <ProfilePage /> */}
+            {/* <PaymentSubscription />
+            <FoodSingleOne />
+            <MealInfo mealName="Meal 1 name" totalCalories={""} />
+            <FoodTable foods={[]} />
             <SearchFood />
-          </div>
-
-          <div>
+            <DailyPlanBreakfast />
             <Subscription />
-          </div>
-
-          <LandingPage />
-        </div> */}
-
-      <div className="container">
-        <div className="nav">
-          <NavLink to={"/"}>
-            <img src={logo_dark_svg} alt="svg logo" className="nav-logo" />
-          </NavLink>
-          <Navigation menuIsOpen={menuIsOpen} setMenuIsOpen={setMenuIsOpen} />
-          <div
-            className="toggle_btn_con"
-            style={{ display: menuIsOpen ? "none" : "block" }}
-            onClick={() => setMenuIsOpen((prev) => !prev)}
-          >
-            {!menuIsOpen && <img src={menu} alt="" className="menu_open" />}
+            <CalculateCalories />
+            <MealPlan /> */}
           </div>
         </div>
-
-        <div className="pages">
-          <ProfilePage />
-          <PaymentSubscription />
-          <FoodSingleOne />
-          <MealInfo mealName="Meal 1 name" totalCalories={""} />
-          <FoodTable foods={[]} />
-          <SearchFood />
-          <DailyPlanBreakfast />
-          <Subscription />
-          <CalculateCalories />
-          <MealPlan />
-        </div>
-      </div>
-    {/* </div > */}
-    </>
   );
-
+  
 }
 
 export default App;
